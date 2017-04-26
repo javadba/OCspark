5edb0b6db8d316127673f7cd19fcd468ccf07d8b
javadba <javadba@gmail.com>
2017-04-25 21:04:30 -0700

    Parameterize gitdir in build.sh

--- tfdma/build.sh
+++ tfdma/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+GITDIR=/git/OCSpark/
+
 echo "[1] Building C header files for java native methods (PcieDMA[Client|Server]) .."
 mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
-javah  -v -stubs -d /git/OCSpark/tfdma/src/main/cpp/includes/ -cp $(cat cp.txt):/git/OCSpark/tfdma/src/main/java org.openchai.tensorflow.api.PcieDMAClient
+javah  -v -stubs -d $GITDIR/tfdma/src/main/cpp/includes/ -cp $(cat cp.txt):$GITDIR/tfdma/src/main/java org.openchai.tensorflow.api.PcieDMAClient
 
 echo "[2] Compiling the C files PcieDMA[Client|Server].c .."
-pushd /git/OCSpark/tfdma/src/main/cpp/ && gcc -dynamiclib -odmaclient.dylib -shared -v -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin" -fpic /git/OCSpark/tfdma/src/main/cpp/org_openchai_tensorflow_api_PcieDMAClient.c; popd
-pushd /git/OCSpark/tfdma/src/main/cpp/ && gcc -dynamiclib -odmaserver.dylib -shared -v -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin" -fpic /git/OCSpark/tfdma/src/main/cpp/org_openchai_tensorflow_api_PcieDMAServer.c; popd
+pushd $GITDIR/tfdma/src/main/cpp/ && gcc -dynamiclib -odmaclient.dylib -shared -v -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin" -fpic $GITDIR/tfdma/src/main/cpp/org_openchai_tensorflow_api_PcieDMAClient.c; popd
+pushd $GITDIR/tfdma/src/main/cpp/ && gcc -dynamiclib -odmaserver.dylib -shared -v -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin" -fpic $GITDIR/tfdma/src/main/cpp/org_openchai_tensorflow_api_PcieDMAServer.c; popd
 echo "**Entry points for dylibs** "
 find . -name \*.dylib | xargs nm -gU | awk '{print $3}'
 
