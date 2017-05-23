4449a774488d96ed6adb40528b33e29eee4520df
javadba <javadba@gmail.com>
2017-05-23 03:29:16 +0000

    Add ARM specific tfdma/build.arm.sh

---
+++ tfdma/build.arm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export GITDIR=/git/OCspark/
+
+# if `mvn package install` already done you can comment out next line
+pushd $GITDIR && mvn package install; popd
+
+echo "[1] Building C header files for java native methods (PcieDMA[Client|Server]) .."
+mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+javah  -v -stubs -d $GITDIR/tfdma/src/main/cpp/includes/ -cp $(cat cp.txt):$GITDIR/tfdma/src/main/java org.openchai.tensorflow.api.PcieDMAClient
+
+echo "[2a] Workaround for -I not working on linux: copy the linux/ include files to its parent dir.."
+cp -p $JAVA_HOME/include/linux/*.h $JAVA_HOME/include
+
+echo "[2] Compiling the C files PcieDMA[Client|Server].c .."
+
+for f in org_openchai_tensorflow_api_PcieDMAClient.c org_openchai_tensorflow_api_PcieDMAServer.c; do  pushd $GITDIR/tfdma/src/main/cpp/ && gcc -v -shared  -I'$JAVA_HOME/include' -I'$JAVA_HOME/include/linux' -odmaserver.so -fpic $GITDIR/tfdma/src/main/cpp/$f; popd; done
+echo "**Entry points for dylibs** "
+find . -name \*.dylib | xargs nm -gU | awk '{print $3}'
+
+echo "[3] Compiling java sources and building release jar.."
+mvn package
