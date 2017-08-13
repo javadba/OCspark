01c944805666f035cfb0540bc3a690abadedd19f
javadba <javadba@gmail.com>
2017-08-12 20:12:21 -0700

    Fixed bug in label_image.sh

--- bin/label_image.sh
+++ bin/label_image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$CURDIR/label_image_bin 2>&1 | grep -v "W tensorflow"
+$CURDIR/label_image $@ 2>&1 | grep -v "W tensorflow"
 #OUTF1=$$
 #OUTF=$OUTF1.log
-#$CURDIR/label_image_bin > $OUTF 2>&1  &
+#$CURDIR/label_image > $OUTF 2>&1  &
 #tail -n 70 -f $OUTF | grep -v "W tensorflow"
