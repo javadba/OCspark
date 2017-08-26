8c16dc219d4985386820ab34484ae449396af181
javadba <javadba@gmail.com>
2017-08-26 03:43:15 +0000

    Revert "Tweaks for build/run on tx1"
    This reverts commit 16192ed45e7cb3044173a8c5b020194b338432a3.

--- bin/buildtf.arm.sh
+++ bin/buildtf.arm.sh
@@ -1,14 +1,13 @@
-#cd $GITDIR/tcpclient
-#mvn package install
-#if [ $? ]; then echo 'mvn package tcplient failed'; exit 127; fi
+cd $GITDIR/tcpclient
+mvn package install
+if [ $? ]; then echo 'mvn package tcplient failed'; exit 127; fi
 cd $GITDIR/tfdma
 ./build.arm.sh  # .build.sh
 
 if [ "$BUILDOK" != "TRUE" ]; then echo 'tfdma build.arm.sh failed'; exit 127; fi
-mvn compile # package  # hhhhhhhhhhinstall
-if (( $? != 0 )); then echo 'mvn package tfdma failed' && popd &&  exit 127; fi
+mvn package install
+if [ "$?" != "0" ]; then echo 'mvn package install tfdma failed'; exit 1; fi
 cd $GITDIR/tf
-mvn compile # package
-if (( $? != 0 )); then echo 'mvn package tf failed' && popd &&  exit 127; fi
+mvn package
+if [ "$?" != "0" ]; then echo 'mvn package tf failed'; exit 1; fi
 echo "** DONE **"
-
---
+++ tfdma/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export GITDIR=/git/OCspark/
+
+echo "[1] Building C header files for java native methods (PcieDMA[Client|Server]) .."
+mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+javah  -v -stubs -d $GITDIR/tfdma/src/main/cpp/includes/ -cp $(cat cp.txt):$GITDIR/tfdma/src/main/java org.openchai.tensorflow.api.PcieDMAClient
+
+echo "[2a] Workaround for -I not working on linux: copy the linux/ include files to its parent dir.."
+cp -p $JAVA_HOME/include/linux/*.h $JAVA_HOME/include
+
+echo "[2] Compiling the C files PcieDMA[Client|Server].c .."
+
+pushd $GITDIR/tfdma/src/main/cpp/ && gcc -v -shared  -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -odmaserver.so -fpic $GITDIR/tfdma/src/main/cpp/org_openchai_tensorflow_api_PcieDMAServer.c; popd
+pushd $GITDIR/tfdma/src/main/cpp/ && gcc -v -shared  -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -odmaclient.so -fpic $GITDIR/tfdma/src/main/cpp/org_openchai_tensorflow_api_PcieDMAClient.c; popd
+echo "**Entry points for dylibs** "
+find . -name \*.so | xargs nm -g | awk '{print $3}'
+
+echo "[3] Compiling java sources and building release jar.."
+#mvn package
